3b57c77699d9814ca761119e330f902db155db5d
timmson <timmson666@mail.ru>
2017-12-20 23:35:35 +0300

    Add some features

--- index.js
+++ index.js
@@ -2,15 +2,37 @@ chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
     let activeTab = tabs[0];
     chrome.tabs.sendMessage(activeTab.id, {msg: "address"}, function (address) {
         $("#house_address").html(address.street + " " + address.building);
-        $.get("http://tipdoma.ru/search_str.php?value=" + decodeURIComponent(address.street), function (data1) {
-            let str1 = data1.match(/change_building\(.*,/i)[0];
-            let id = str1.substr(16, str1.length - 3);
-            $.get("http://tipdoma.ru/search_bld.php?id=" + id + "&value=" + decodeURIComponent(address.street), function (data2) {
-                let str2 = data2.replace(/href="series/ig, 'href="http://tipdoma.ru/series');
-                $("#house_type").html(str2);
-            });
+        getStreetId(address.street, function (err, streetId) {
+            if (err) {
+                console.error(err);
+            } else {
+                getStreetData({
+                    id: streetId,
+                    name: address.street
+                }, function (err, data) {
+                    if (err) {
+                        console.error(err);
+                    } else {
+                        $("#house_type").html(data);
+                    }
+                });
+            }
         });
     });
 });
 
+function getStreetId(streetName, callback) {
+    $.get("http://tipdoma.ru/search_str.php?value=" + decodeURIComponent(streetName), function (data) {
+        let str = $("a", data).first().attr("onclick");
+        let id = str.split("(")[1].split(",")[0];
+        return callback(null, id);
+    });
+}
 
+function getStreetData(street, callback) {
+    $.get("http://tipdoma.ru/search_bld.php?id=" + street.id + "&value=" + decodeURIComponent(street.name), function (data) {
+        data = data.replace(/href="series/ig, 'href="http://tipdoma.ru/series');
+        $("")
+        callback(null, data);
+    });
+}
