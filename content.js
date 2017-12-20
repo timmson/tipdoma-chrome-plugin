e2734b5555c5062c84f24ac2b94f0809bd0b7956
timmson <timmson666@mail.ru>
2017-12-20 22:06:32 +0300

    Add some features

--- content.js
+++ content.js
@@ -3,9 +3,17 @@ $(document).ready(function () {
     $("address > a").each(function() {
         address.push($(this).html());
     });
-    console.log(address);
-    chrome.runtime.sendMessage({
-        msg: "address",
-        data: address
-    });
+
+    address = address.join().split(",");
+    chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+        console.debug("Message received");
+        if (request.msg === "address") {
+            sendResponse({
+                city: address[0] + ", " + address[1],
+                district: address[2],
+                street: address[3],
+                building: address[4]
+            });
+        }
+    })
 });
--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "http://tipdoma.ru/search_str.php?value=%D0%90%D0%B2%D0%B0%D0%BD%D0%B3%D0%B0%D1%80%D0%B4%D0%BD%D0%B0%D1%8F", true);
-xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4) {
-        var content = document.getElementById('content');
-        console.log(xhr.responseText);
-        content.innerHTML = xhr.responseText;
-    }
-}
-xhr.send();
+chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
+    let activeTab = tabs[0];
+    chrome.tabs.sendMessage(activeTab.id, {msg: "address"}, function (address) {
+        $("#house_address").html(address.street + " " + address.building);
+        $.get("http://tipdoma.ru/search_str.php?value=" + decodeURIComponent(address.street), function (data1) {
+            let str1 = data1.match(/change_building\(.*,/i)[0];
+            let id = str1.substr(16, str1.length - 3);
+            $.get("http://tipdoma.ru/search_bld.php?id=" + id + "&value=" + decodeURIComponent(address.street), function (data2) {
+                let str2 = data2.replace(/href="series/ig, 'href="http://tipdoma.ru/series');
+                $("#house_type").html(str2);
+            });
+        });
+    });
+});
+
+
