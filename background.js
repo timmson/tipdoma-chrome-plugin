65b2e8fa573f572c4abae2f4ab5873ec0be26a5e
timmson <timmson666@mail.ru>
2017-12-21 22:57:16 +0300

    #1

--- background.js
+++ background.js
@@ -1,40 +1,70 @@
-/*var addresses = {};
-var selectedAddress = null;
-var selectedId = null;
-
-function updateAddress(tabId) {
-    chrome.tabs.sendRequest(tabId, {}, function(address) {
-        addresses[tabId] = address;
-        if (!address) {
-            chrome.pageAction.hide(tabId);
-        } else {
-            chrome.pageAction.show(tabId);
-            if (selectedId === tabId) {
-                updateSelected(tabId);
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+    if (message.msg === "address") {
+        let address = message.address;
+        console.debug(address.street + " " + address.building);
+        let streetName = address.street.replace(/проезд/i, "");
+        getStreetId(streetName, function (err, streetId) {
+            if (err) {
+                console.error(err);
+            } else {
+                getBuildingData({
+                    streetId: streetId,
+                    streetName: address.street,
+                    number: address.building
+                }, function (err, data) {
+                    if (err) {
+                        console.error(err);
+                    } else {
+                        sendResponse(data);
+                    }
+                });
             }
-        }
+        });
+    } else if (message.msg === "plan") {
+        getHouseTypeByUrl(message.url, function (err, houseType) {
+            console.debug(houseType);
+            sendResponse(houseType);
+        })
+    }
+    return true;
+});
+
+function getStreetId(streetName, callback) {
+    $.get("http://tipdoma.ru/search_str.php?value=" + decodeURIComponent(streetName), function (data) {
+        let str = $("a", data).first().attr("onclick");
+        let id = str.split("(")[1].split(",")[0];
+        return callback(null, id);
     });
 }
 
-function updateSelected(tabId) {
-    selectedAddress = addresses[tabId];
-    if (selectedAddress) {
-        chrome.pageAction.setTitle({tabId:tabId, title:selectedAddress});
-    }
+function getBuildingData(building, callback) {
+    $.get("http://tipdoma.ru/search_bld.php?id=" + building.streetId + "&value=" + decodeURIComponent(building.streetName), function (data) {
+        let buildings = [];
+        $("tbody tr", data).each(function () {
+            let address = $(this).find("td:nth-child(2) a").html();
+            let buildNumber = building.number.replace(/К/i, " корп.").replace(/С/i, " стр.");
+            if (address.search(buildNumber) >= 0) {
+                buildings.push({
+                    address: address,
+                    material: $(this).find("td:nth-child(3)").html(),
+                    floorsCount: $(this).find("td:nth-child(4)").html(),
+                    year: $(this).find("td:nth-child(5)").html(),
+                    series: {
+                        name: $(this).find("td:nth-child(6) a").html(),
+                        url: "http://tipdoma.ru/" + $(this).find("td:nth-child(6) a").attr("href")
+                    }
+                })
+            }
+        });
+        callback(null, buildings);
+    });
 }
 
-chrome.tabs.onUpdated.addListener(function(tabId, change, tab) {
-    if (change.status == "complete") {
-        updateAddress(tabId);
-    }
-});
-
-chrome.tabs.onSelectionChanged.addListener(function(tabId, info) {
-    selectedId = tabId;
-    updateSelected(tabId);
-});
-
-// Ensure the current selected tab is set up.
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    updateAddress(tabs[0].id);
-});*/
+function getHouseTypeByUrl(url, callback) {
+    $.get(url, function (data) {
+        callback(null, {
+            photo: "http://tipdoma.ru/" + $("#carousel-photo > div > div > img", data).attr("src"),
+            plan: "http://tipdoma.ru/" + $("#carousel-plan > div > div > img", data).attr("src")
+        });
+    });
+}
--- content.js
+++ content.js
@@ -1,19 +1,57 @@
 $(document).ready(function () {
     let address = [];
-    $("address > a").each(function() {
+    $("address > a").each(function () {
         address.push($(this).html());
     });
 
     address = address.join().split(",");
-    chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-        console.debug("Message received");
-        if (request.msg === "address") {
-            sendResponse({
-                city: address[0] + ", " + address[1],
-                district: address[2],
-                street: address[3],
-                building: address[4]
-            });
+
+    chrome.runtime.sendMessage({
+        msg: "address", address: {
+            city: address[0] + ", " + address[1],
+            district: address[2],
+            street: address[3],
+            building: address[4]
         }
-    })
+    }, function (response) {
+        //console.log(response);
+        //$(getDescriptionTable(response)).insertAfter("address");
+        $("[data-reactid=230]").html(getDescriptionTable(response));
+        $("p#tipdoma_section_enable").click(function () {
+            $("div#tipdoma_section").slideToggle();
+        });
+    });
 });
+
+function getDescriptionTable(buildings) {
+    let out ="<p id=\"tipdoma_section_enable\">Tipdoma.ru</p><div id=\"tipdoma_section\">";
+    out += "<table class=\"tipdoma_data\">";
+    out += "<tr><th>Address</th><th>Material</th><th>Floors</th><th>Built</th><th>Series</th></tr>";
+    for (let i = 0; i < buildings.length; i++) {
+        out += "<tr>";
+        out += "<td>" + buildings[i].address + "</td>";
+        out += "<td>" + buildings[i].material + "</td>";
+        out += "<td>" + buildings[i].floorsCount + "</td>";
+        out += "<td>" + buildings[i].year + "</td>";
+        out += "<td>";
+        if (buildings[i].series.name !== undefined) {
+            out += "<a target=\"_blank\" href=\"" + buildings[i].series.url + "\">" + buildings[i].series.name + "</a>";
+        } else {
+            out += "Individual project";
+        }
+        out += "</td>";
+        out += "</tr>";
+        if (buildings[i].series.name !== undefined) {
+            out += "<tr><td colspan=\"5\"><a id=\"plan" + i + "\" target=\"_blank\">&nbsp;</a></td></tr>";
+            chrome.runtime.sendMessage({msg: "plan", url: buildings[i].series.url}, function (houseType) {
+                console.log(houseType);
+                $("#plan" + i).attr("href", houseType.plan);
+                $("#plan" + i).append("<img class=\"tipdoma_img\" src=\"" + houseType.plan + "\"/>");
+            });
+
+        }
+    }
+
+    out += "</table></div>";
+    return out;
+}
